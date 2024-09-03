c48e1f09ab15da3749ae47856156e8799bb81915
quercusfursiformis <sricks393@gmail.com>
2024-09-03 13:40:47 -0600

    Defined leaflet search areas and implemented LogLeaflet.has_impossible_mult_electr()

--- structure/comleaflet.py
+++ structure/comleaflet.py
@@ -1,14 +1,16 @@
 from leaflet import Leaflet
 
+NUM_LINES_IN_HEADER_SEARCH: int = 5
+
 class ComLeaflet(Leaflet):
     def get_nprocshared(self) -> int:
-        for line in self.head(5).splitlines():
+        for line in self.head(NUM_LINES_IN_HEADER_SEARCH).splitlines():
             if "nprocshared" in line.lower():
                 return int(line.split('=')[-1])
         return None
 
     def get_mem(self) -> str:
-        for line in self.head(5).splitlines():
+        for line in self.head(NUM_LINES_IN_HEADER_SEARCH).splitlines():
             if "mem" in line.lower():
                 return line.split('=')[-1]
         return None
--- structure/logleaflet.py
+++ structure/logleaflet.py
@@ -1,8 +1,17 @@
 from leaflet import Leaflet
 
+NUM_LINES_IN_COMPLETION_SEARCH: int = 5
+NUM_LINES_IN_ERROR_SEARCH: int = 10
+
 class LogLeaflet(Leaflet):
     def is_completed(self) -> bool:
-        for line in self.tail(5).splitlines():
+        for line in self.tail(NUM_LINES_IN_COMPLETION_SEARCH).splitlines():
             if "Normal termination of Gaussian" in line:
+                return True
+        return False
+    
+    def has_impossible_mult_electr(self) -> bool:
+        for line in self.tail(NUM_LINES_IN_ERROR_SEARCH).splitlines():
+            if all(val in line for val in ["The combination of multiplicity", "electrons is impossible"]):
                 return True
         return False
